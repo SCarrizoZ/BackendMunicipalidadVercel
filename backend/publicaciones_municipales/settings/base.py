961435f541f8dd60235c9b7c3c3150224b5063d4
Sebastián Carrizo <sebastian.carrizo03@inacapmail.cl>
2024-10-14 18:34:30 -0300

    Cambio en las rutas de los archivos estaticos

--- backend/publicaciones_municipales/settings/base.py
+++ backend/publicaciones_municipales/settings/base.py
@@ -142,7 +142,7 @@ DEBUG = (
 
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
